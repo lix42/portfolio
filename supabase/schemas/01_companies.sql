60a68b1977323b6cda4f012f7f0f131a4fa1c592
Li Xu <i@xuli.dev>
2025-05-31 14:52:29 -0700

    ingest companies table

--- supabase/schemas/01_companies.sql
+++ supabase/schemas/01_companies.sql
@@ -1,8 +1,9 @@
 create table companies (
   id uuid primary key default gen_random_uuid(),
-  name text not null unique,
+  name text not null,
   start_time date not null,
   end_time date,
   title text not null,
-  description text not null
+  description text not null,
+  unique (name, start_time)
 );
