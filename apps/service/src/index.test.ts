f489b5c40249f76b3eddae26e464143b2c1bba4a
Li Xu <i@xuli.dev>
2025-07-03 13:18:14 -0700

    set up chat route

--- apps/service/src/index.test.ts
+++ apps/service/src/index.test.ts
@@ -5,6 +5,5 @@ describe('Example', () => {
   test('GET /', async () => {
     const res = await app.request('http://localhost/')
     expect(res.status).toBe(200)
-    expect(res.headers.get('x-powered-by')).toBe('Hono')
   })
 })
--- apps/service/src/index.ts
+++ apps/service/src/index.ts
@@ -1,21 +1,7 @@
 import { Hono } from "hono";
-import { basicAuth } from "hono/basic-auth";
-import { etag } from "hono/etag";
-import { poweredBy } from "hono/powered-by";
-import { prettyJSON } from "hono/pretty-json";
+import chat from "./chat";
 
 const app = new Hono();
-
-// Mount Builtin Middleware
-app.use("*", poweredBy());
-
-// Add X-Response-Time header
-app.use("*", async (c, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  c.header("X-Response-Time", `${ms}ms`);
-});
 
 // Custom Not Found Message
 app.notFound((c) => {
@@ -30,5 +16,6 @@ app.onError((err, c) => {
 
 // Routing
 app.get("/", (c) => c.text("Hono!!"));
+app.route("/chat", chat);
 
 export default app;
